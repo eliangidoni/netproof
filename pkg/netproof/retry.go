c2f4ad32fcd55184644bf204ffd7308ab9d23f73
Elian Gidoni <elianmdp@gmail.com>
2025-07-05 16:16:17 +0200

    support token bucket

--- pkg/netproof/retry.go
+++ pkg/netproof/retry.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"math"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -15,6 +16,10 @@ type Retry struct {
 	maxWait     time.Duration
 	scaleFactor time.Duration
 	t           timer
+	maxTokens   float64
+	tokens      float64
+	refillRate  float64
+	lock        sync.Mutex
 }
 
 // NewRetry returns a new Retry, maxWait can be zero (disabled).
@@ -32,13 +37,52 @@ func WithBackoff(maxCount int, maxWait time.Duration) *Retry {
 	return &Retry{maxCount: maxCount, maxWait: maxWait, scaleFactor: time.Second, t: realTimer{}}
 }
 
+// WithBucket returns a new Retry with token bucket + exponential backoff + jitter.
+// maxWait can be zero (disabled).
+func WithBucket(maxTokens float64, refillRate float64, maxCount int, maxWait time.Duration) *Retry {
+	return &Retry{maxCount: maxCount, maxWait: maxWait, scaleFactor: time.Second, t: realTimer{},
+		maxTokens: maxTokens, refillRate: refillRate, tokens: maxTokens,
+	}
+}
+
+func (r *Retry) consumeToken() bool {
+	if r.maxTokens == 0 {
+		return true
+	}
+
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	if r.tokens >= 1 {
+		r.tokens--
+		return true
+	}
+	return false
+}
+
+func (r *Retry) produceToken() {
+	if r.maxTokens == 0 {
+		return
+	}
+
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	r.tokens = math.Max(r.maxTokens, r.tokens+r.refillRate)
+}
+
 // Retry retries the given function f() up to maxCount times.
 // It stops after maxWait or when f() returns ErrStop.
 func (r *Retry) Retry(f func() error) error {
 	var err error
 	startTime := r.t.Now()
 	for i := 0; i < r.maxCount; i++ {
-		if err = f(); errors.Is(err, ErrStopRetry) || err == nil {
+		if err = f(); err == nil {
+			r.produceToken()
+			return nil
+		}
+		if !r.consumeToken() {
+			return err
+		}
+		if errors.Is(err, ErrStopRetry) {
 			return err
 		}
 		if r.maxWait > 0 && r.t.Since(startTime) >= r.maxWait {
